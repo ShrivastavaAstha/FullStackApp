81c3f0b4b804956574951372cba3b00004ce3675
Astha <astha2333@gmail.com>
2024-01-28 22:52:51 +0530

    update

--- app_frontend/src/component/Profile.js
+++ app_frontend/src/component/Profile.js
@@ -1,33 +1,79 @@
-import { useRef } from "react";
-import { Link } from "react-router-dom";
+import { useRef, useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 import "./Profile.css";
 import logoimage from "./logo.png";
 
 const Profile = () => {
   const navRef = useRef();
+  const navigate = useNavigate();
+  const [userdata, setuserdata] = useState();
 
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get("/logout");
+      if (response.data.success) {
+        toast.success("You were logged out successfully");
+        navigate("/Login");
+      }
+    } catch (error) {
+      console.log(error);
+      if (
+        error &&
+        error.response &&
+        error.response.data &&
+        error.response.data.error
+      ) {
+        toast.error(error.response.data.error);
+      }
+    }
+  };
   const showNavbar = () => {
     navRef.current.classList.toggle("responsive_nav");
   };
 
   return (
-    <header>
-      <img className="logo" src={logoimage} alt="Logo" />
-      <nav ref={navRef}>
-        <h3 style={{ borderBottom: "white solid 2px" }}>LIFESYNC</h3>
-        <Link to="/">LogOut</Link>
-        <Link to="/Logout">Login</Link>
-        <Link to="/Signup">Signup</Link>
+    <>
+      <header>
+        <img className="logo" src={logoimage} alt="Logo" />
+        <nav ref={navRef}>
+          <h3 style={{ borderBottom: "white solid 2px" }}>LIFESYNC</h3>
+          <Link to="/">Login</Link>
+          <button
+            type="button"
+            onClick={() => handleLogout()}
+            style={{
+              backgroundColor: "transparent",
+              color: "white",
+              fontSize: "22px",
+            }}
+          >
+            Logout
+          </button>
+          <Link to="/Signup">Signup</Link>
 
-        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+            <MenuIcon />
+          </button>
+        </nav>
+        <button className="nav-btn" onClick={showNavbar}>
           <MenuIcon />
         </button>
-      </nav>
-      <button className="nav-btn" onClick={showNavbar}>
-        <MenuIcon />
-      </button>
-    </header>
+      </header>
+      <ToastContainer />
+      <div className="mainbody">
+        <h1>Hello! {userdata.name}, Welcome .</h1>
+        {/* <p>
+          Age: {new Date().getFullYear() - new Date(userdata.dob).getFullYear()}{" "}
+          Years
+        </p> */}
+        <p>Email: {userdata.email}</p>
+        <p>Contact Number: {userdata.contact}</p>
+      </div>
+    </>
   );
 };
 
